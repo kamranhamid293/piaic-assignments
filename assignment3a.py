03a1180b67071a7d1e01a1661a23fd41342b37ca
Kamran Hamid <kamranhamid293@yahoo.com>
2024-08-11 11:44:43 +0500

    Assignment 3 - PIAIC
    Assignment 3 for PIAIC

---
+++ assignment3a.py
@@ -0,0 +1,86 @@
+<<<<<<< HEAD
+print("Hello World")
+=======
+message:str = "Hello World"
+print(message)
+
+message:str = "Hello New World"
+print(message)
+
+name = "kamran"
+print(name)
+
+name = "kamran"
+print(name.upper())
+
+name = "KAMRAN"
+print(name.lower())
+
+name = "kamran"
+print(name.capitalize())
+
+famous_person_name:str = "Alberg Einstein"
+quote:str = """ once said " A person who never made a mistake never tried anything new." """
+message_quote = famous_person_name + quote
+print(message_quote)
+
+
+name2 = "Ali Ahmed     "
+print(name2.rstrip())
+
+name2 = "      Ali Ahmed"
+print(name2.lstrip())
+
+name2 = "      Ali Ahmed       "
+print(name2.strip())
+
+
+x:int = 5
+y:int = 10
+z:int = 15
+
+sum:int = (x+y+z)
+print(sum)
+
+print("x =", x)
+print("y =", y)
+print("z =", z)
+
+print("After swap")
+
+print("x =", y)
+print("y =", z)
+print("z =", x)
+
+color = "Black"
+print(color)
+
+color = "Yellow"
+print(color)
+
+pet_name = "Buddy"
+print(pet_name)
+
+pet_name = "Max"
+print(pet_name)
+
+Sunshine = "Hi"
+print(Sunshine)
+
+score:int = 100
+print(score)
+
+score:int = 50
+print(score)
+
+city:str = "Lahore"
+print(city)
+
+text:str = "python programming"
+print(text.title())
+
+temperature:int = 25
+print(f"The current temperature is {temperature} degrees.")
+
+poem:str = """This is my world. 
+I like my world very much."""
